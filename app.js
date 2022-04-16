b4c1c810db395118cee4eeef6b6394bd4c88b1c4
fahad shaikh <fahadshaikh1337@gmail.com>
2022-04-17 00:27:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb+srv://admin-fahad:fahad@cluster0.gbtr5.mongodb.net/urlchecker");
+
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+const urlSchema = new mongoose.Schema( {
+    name: String,
+    website: String
+    
+  });
+  
+  const Url = mongoose.model('Url', urlSchema);
+
+app.get('/',function(req,res) {
+    
+    res.sendFile(__dirname + "/index.html");
+
+  });
+
+
+app.post("/", function(req, res){
+
+ 
+
+
+
+
+
+  const url = {
+    name: req.body.companyName,
+    link: req.body.website
+
+
+  };
+
+  Url.findOne({ website: url.link }, function(err, foundCompany) {
+  if(foundCompany) {
+    console.log("found in db");
+    res.redirect("/notaccepted")
+
+  } else {
+
+    const newUrl = new Url ({
+      name: url.name,
+      website: url.link
+  
+    })
+  
+    newUrl.save();
+  
+    
+    
+
+
+
+    console.log("not found in db");
+    res.redirect("/accepted")
+  }
+
+
+  });
+
+  
+
+});
+
+
+app.get('/accepted',function(req,res) {
+    
+  res.sendFile(__dirname + "/notfound.html");
+
+});
+
+app.get('/notaccepted',function(req,res) {
+    
+  res.sendFile(__dirname + "/found.html");
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
